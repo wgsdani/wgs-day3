51337c205d689de3d48dea865a9e6ee1865b1914
dani mulyana <daniwgs92@gmail.com>
2022-09-07 16:48:44 +0700

    YARGS List & Detailed

--- app.js
+++ app.js
@@ -1,5 +1,66 @@
-const validator = require('validator');
+// const validator = require('validator');
 
-console.log(validator.isEmail('daniwgs92@gmail.com'));
-console.log(validator.isMobilePhone('08996988449','id-ID'));
+// console.log(validator.isEmail('daniwgs92@gmail.com'));
+// console.log(validator.isMobilePhone('08996988449','id-ID'));
+
+const { command } = require('yargs');
+const yargs = require('yargs')
+// console.log(yargs.argv);
+
+const func = require('./function')
+
+
+yargs.command({
+	command:'add',
+	describe:'add new contact',
+	builder:{
+		name:{
+			describe:'Contact Name',
+			demandOption:true,
+			type:'string',
+		},
+		email:{
+			describe:'Contact Email',
+			demandOption:false,
+			type:'string',
+		},
+		mobile:{
+			describe:'Contact mobile phone number',
+			demandOption:true,
+			type:'string',
+		},
+	},
+	handler(argv){
+		func.getcontacts(argv.name, argv.email, argv.mobile);
+	}
+});
+
+
+	yargs.command({
+		command:'list',
+		describe:'List contact data',
+	
+	handler(){
+		func.listContact();
+	}
+});
+
+
+	yargs.command({
+		command:'detail',
+		describe:'Detail contact data',
+		builder:{
+			name:{
+				describe:'Contact Name',
+				demandOption:true,
+				type:'string',
+			},
+		},
+	
+	handler(argv){
+		func.detailContact(argv.name);
+	}
+});
+
+yargs.parse();
 
--- function.js
+++ function.js
@@ -1,14 +1,16 @@
 
-const readline = require("readline");
+// const readline = require("readline");
 const fs = require('fs');
 const { resolve } = require("path");
 const { rejects } = require("assert");
+const validator = require('validator');
 
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
 
-});
+// const rl = readline.createInterface({
+// 	input: process.stdin,
+// 	output: process.stdout
+
+// });
 
 // membuat folder data apabila tidak ada
 	const data = "./data";
@@ -24,29 +26,78 @@ const rl = readline.createInterface({
 		fs.writeFileSync(filepath,`[]`);
 	}
 
-// pertanyaan
-	const pertanyaan = (ask) => {
-		return new Promise((resolve,rejects)=>{
-			rl.question(ask, (jawaban)=>{
-				resolve (jawaban)
-			})
-		})
-	}
+// // pertanyaan
+// 	const pertanyaan = (ask) => {
+// 		return new Promise((resolve,rejects)=>{
+// 			rl.question(ask, (jawaban)=>{
+// 				resolve (jawaban)
+// 			})
+// 		})
+// 	}
 
-
-
-// menyimpan data ke dalam folder data
-	const getcontacts = (name,number,email) => {
-		const contact = {name,number,email};
-		const file = fs.readFileSync(filepath,'utf-8');
+// Load Contact
+const loadContact=()=> {
+	const file = fs.readFileSync(filepath,'utf-8');
 		const contacts = JSON.parse(file);
-		contacts.push(contact);
 
+		return contacts;
+}
+
+// List Contact
+const listContact=()=>{
+	const contacts = loadContact();
+	console.log('Contact List : ');
+	contacts.forEach((contact,i) => {
+		console.log(`${i+1}.${contact.name}-${contact.mobile}`);
+	});
+};
+
+// Detail Contact
+const detailContact=(name) => {
+	const contacts = loadContact();
+	const detailContact = contacts.find((contact) => contact.name === name);
+	if(detailContact){
+		console.log('This is detail contact');
+		console.log('Contact Detail : ');
+		console.log(detailContact.name);
+		console.log(detailContact.email);
+		console.log(detailContact.mobile);
+	}else {
+		console.log('Detailed data is invalid');
+		return false;
+	};
+
+
+
+}
+// menyimpan data contacts
+	const getcontacts = (name,email,mobile) => {
+		const contact = {name,email,mobile};
+		const contacts = loadContact() 
+
+		const duplicateName=contacts.find((contact)=> contact.name === name);
+		if(duplicateName){
+			console.log('Contact name is already recorded. Use another contact name');
+			return false;
+		}
+		if(email){
+
+		if(!validator.isEmail(email)){
+			console.log('Email format is invalid !');
+
+			return false;
+		}
+	}
+		if(!validator.isMobilePhone(mobile, 'id-ID')){
+			console.log('Mobile Phone is invalid !');
+		}
+		
+		contacts.push(contact);
 		fs.writeFileSync(filepath, JSON.stringify(contacts));
 	
 			console.log(`Thank youu ${name}, for ur entering the data !!`);
 
-rl.close();
+// rl.close();
 }
 
-module.exports = { getcontacts, pertanyaan}
+module.exports = {getcontacts, listContact, detailContact}
